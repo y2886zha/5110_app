10918976b50ef010e4290dd0714cd401a3a3ca16
y2886zha <62088738+y2886zha@users.noreply.github.com>
2021-10-22 02:08:04 +0800

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -26,10 +26,10 @@ class ImoForm(forms.Form):
     imo = forms.IntegerField(label='IMO Number', min_value=1111111, max_value=9999999)
     ship_name = forms.CharField(max_length=64)
     technical_efficiency_number = forms.DecimalField(label='EEDI', max_digits=6, min_value=0, required=False)
-    # ship_type = forms.DecimalField(choices=get_choices('ship_type'), required=False)
-    # issue_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}), required=False)
+    ship_type = forms.DecimalField(choices=get_choices('ship_type'), required=False)
+    issue_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}), required=False)
 <<<<<<< HEAD
-    # expire_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}), required=False)
+    expire_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}), required=False)
 =======
-    # expire_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}), required=False)
+    expire_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}), required=False)
 >>>>>>> 0a6173c9196d2ad960551121cfbe846763f92488
