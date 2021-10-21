4cd54e87ccb03a29b42d7e0f84e755be55f8b57a
张一沛 <zhang621108@zhangyipeideMacBook-Air.local>
2021-10-21 22:41:15 +0800

    First commit

--- core/urls.py
+++ core/urls.py
@@ -9,9 +9,11 @@ admin.autodiscover()
 urlpatterns = [
     path('', app.views.index, name='index'),
     path('db/', app.views.db, name='db'),
+    path('aggregation/', app.views.aggregation, name='aggregation'),
     path('emissions/', app.views.emissions, name='emissions'),
     path('emissions/<int:page>', app.views.emissions, name='emissions'),
     path('emissions/imo/', app.views.emission_detail, name='emission_detail'),
     path('emissions/imo/<int:imo>', app.views.emission_detail, name='emission_detail'),
     path('admin/', admin.site.urls),
+    path('visual/', app.views.visual, name='visual'),
 ]
